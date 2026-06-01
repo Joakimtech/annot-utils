07864f0314d7ad5205ffab0b9c2b237bc49266ea
Joakim Kiugi <kiugijoakim@gmail.com>
2026-06-01 23:45:41 +0300

    Add setup.py for annot-utils package

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="annot-utils",
+    version="0.1.0",
+    author="Your Name",
+    description="Annotation format converter and quality checker",
+    packages=find_packages(),
+    install_requires=[
+        "numpy>=1.24.0",
+        "shapely>=2.0.0",
+        "Pillow>=10.0.0",
+        "click>=8.1.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "annot-utils=annot_utils.cli:main",
+        ],
+    },
+    python_requires=">=3.8",
+)
