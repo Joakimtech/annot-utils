078658302ec8d875f3620c109bb7e04265a19d82
Joakim Kiugi <joakim.muiruri24@students.dkut.ac.ke>
2026-06-08 00:31:42 +0300

    Update author name in _init_.py

--- annot_utils/_init_.py
+++ annot_utils/_init_.py
@@ -3,7 +3,7 @@ Annotation Utilities - Convert, validate, and analyze annotation formats
 """
 
 __version__ = "0.1.0"
-__author__ = "Your Name"
+__author__ = "Joakimtech"
 
 from .converter import AnnotationConverter
 from .validator import AnnotationValidator
@@ -11,6 +11,6 @@ from .agreement import AgreementCalculator
 
 __all__ = [
     "AnnotationConverter",
-    "AnnotationValidator", 
+    "AnnotationValidator",
     "AgreementCalculator",
 ]
